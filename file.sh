f4fae0bef633f2c94cc8b598ef93b4051d13ffa4
Kirill Sverchkov <107706691+ksverchkov@users.noreply.github.com>
2024-09-08 23:50:36 +0300

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,1 +1,3 @@
-https://github.com/pop-os/linux-firmware/raw/4ee8dc918da688d31cac9f049c473cf982f722f6/amdgpu/gc_11_0_4_mes_2.bin
+wget https://github.com/pop-os/linux-firmware/raw/4ee8dc918da688d31cac9f049c473cf982f722f6/amdgpu/gc_11_0_4_mes_2.bin
+wget https://github.com/pop-os/linux-firmware/blob/3cc9cad0be2022a88259686571fa0a1b3bc37a93/amdgpu/gc_11_0_3_mes1.bin
+wget https://github.com/pop-os/linux-firmware/raw/3cc9cad0be2022a88259686571fa0a1b3bc37a93/amdgpu/gc_11_0_4_mes.bin
